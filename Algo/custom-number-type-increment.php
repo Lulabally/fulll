ef9c9208ff254d449256e928f91275a00b11483f
Lulabally <delcambre.julia@gmail.com>
2021-05-17 15:58:55 +0200

    update Algo/Custom number type increment

--- Algo/custom-number-type-increment.php
+++ Algo/custom-number-type-increment.php
@@ -1,17 +1,17 @@
 <?php
 
-print_r(increment([1, 9, 9, 9, 9]));
+print_r(increment([9, 9, 9, 9]));
 
 function increment(array $number): array
 {
     for ($i = count($number) - 1; $i >= 0; --$i) {
-        if (9 === $number[$i]) {
-            $number[$i] = 0;
-            (0 === $i) ?? array_unshift($number, 1);
-        } else {
-            ++$number[$i];
+        $number[$i] = (9 === $number[$i]) ? 0 : ++$number[$i];
+        if (0 != $number[$i]) {
+            break;
         }
     }
+
+    (0 === $number[0]) ? array_unshift($number, 1) : '';
 
     return $number;
 }
